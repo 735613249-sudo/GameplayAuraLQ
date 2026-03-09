00606d96392cd0e2cc012a0bd29976268b6fc794
unknown <735613249@qq.com>
2026-03-09 18:07:37 +0800

    Attribute Accessors

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -15,4 +15,8 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase
 	GENERATED_BODY()
 public:
 	AAuraCharacter();				// 声明公有构造函数，用于初始化角色属性
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+private:
+	void InitAbilityActorInfo();
 };
