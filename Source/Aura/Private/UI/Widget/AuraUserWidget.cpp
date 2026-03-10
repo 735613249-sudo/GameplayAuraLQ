0296e5cdcf611b35065e95427718aae8f26a4da3
unknown <735613249@qq.com>
2026-03-10 18:33:42 +0800

    Aura HUD

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Li Qian
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Li Qian
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
